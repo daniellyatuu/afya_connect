96d9fdd5fe389fa3fa92a48d004b633f46295591
Lyatuu <daniellyatuu@gmail.com>
2020-10-26 21:39:39 +0300

    update

--- afya_connect/settings.py
+++ afya_connect/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -122,12 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#    BASE_DIR / 'static'
-# ]
 STATIC_ROOT = BASE_DIR / 'static/'
-print(STATIC_ROOT)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-print('MEDIA_ROOT')
